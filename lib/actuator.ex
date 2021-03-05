4fe256067f42e741e2cae326a998b25cc59242e4
Daniel Bjerkehagen <danielbb@stud.ntnu.no>
2021-03-05 19:38:56 +0100

    Move door and motor functionality in to Actuator module, remember to de-comment positioner interaction :)

--- lib/actuator.ex
+++ lib/actuator.ex
@@ -13,8 +13,54 @@ defmodule Actuator do
         Door.door_open_wait_until_closing
 
       {:actuator, {:motor_order, motor_direction}, pid_parent} ->
-        Motor.order_direction(motor_direction)
+        :ok = Motor.change_direction(motor_direction)
+        #send(:positioner, {:actuator, {:direction_change, motor_direction}, self})
     end
     handle_messages(pid_parent)
+  end
+end
+
+
+
+defmodule Door do
+  @moduledoc """
+  Opens door, and closes it after door_wait_for_obstruction_time_ms as long as obstruction isn't active.
+  Call Door.door_open_wait_until_closing() to do this, man.
+  """
+
+  def door_open_wait_until_closing() do
+    door_open()
+
+    :timer.sleep(Constants.door_wait_for_obstruction_time_ms)
+    :door_free_to_close = door_wait_until_free_to_close()
+
+    door_close()
+  end
+
+  defp door_open() do
+    Driver.set_door_open_light(:on)
+  end
+
+  defp door_close() do
+    Driver.set_door_open_light(:off)
+  end
+
+  defp door_wait_until_free_to_close() do
+    case Driver.get_obstruction_switch_state() do
+      :active ->
+        door_wait_until_free_to_close()
+
+      :inactive ->
+        :door_free_to_close
+    end
+
+  end
+
+end
+
+
+defmodule Motor do
+  def change_direction(motor_direction) do
+    Driver.set_motor_direction(motor_direction)
   end
 end
