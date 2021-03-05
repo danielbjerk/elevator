658efbbe3791c289f3751bf7e436330e6e466a07
Daniel Bjerkehagen <danielbb@stud.ntnu.no>
2021-03-05 20:03:26 +0100

    Fixed positioner's interface not matching that of hardware

--- lib/positioner.ex
+++ lib/positioner.ex
@@ -1,6 +1,6 @@
 defmodule Positioner do
 
-    defp notify_on_position_change(parent_pid, old_position) do  # Notifies parent_pid of position where position is of type {:moving_up/:moving_down/:stationary, floor}.
+    defp notify_on_position_change(parent_pid, old_position) do  # Notifies parent_pid of position where position is of type {:up/:down/:stop, floor}.
         case old_position do
             {old_floor, old_direction} when old_floor != :unknown ->    # Position (floor and direction) is known.
                 send(parent_pid, old_position)
@@ -8,16 +8,16 @@ defmodule Positioner do
 
             {old_floor, old_direction} ->   # At least one of floor and direction is not known.
                 receive do
-                    direction when direction in [:moving_up, :moving_down, :stationary] ->
+                    direction when direction in [:up, :down, :stop] ->
                         notify_on_position_change(parent_pid, {old_floor, direction})
                     floor ->
                         notify_on_position_change(parent_pid, {floor, old_direction})
                 end
         end
-    end       
+    end
 
     def start_positioner(parent_pid) do
-        spawn(fn -> notify_on_position_change(parent_pid, {0, :stationary}) end)
+        spawn(fn -> notify_on_position_change(parent_pid, {0, :stop}) end)
         |> Process.register(:positioner)
 
         Floor.start_listening_for_floor_changes(:positioner, 0)
